625aae9cae5129e558783658f46114d267b90fdb
Daniel Dalcastagne <dalcastagned@gmail.com>
2022-02-10 19:07:01 -0300

    improvements SchedulingCard components

--- src/components/SchedulingCard/index.js
+++ src/components/SchedulingCard/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import moment from 'moment';
+import 'moment/locale/pt-br'
 
 import * as S from './elements'
 
-const SchedualingCard = ({ status, title, date, hour, description }) => {
+const SchedualingCard = ({
+    status,
+    title,
+    day,
+    initialHour,
+    finalHour,
+    description
+}) => {
 
     const color = (
         status === "done"
@@ -18,8 +27,8 @@ const SchedualingCard = ({ status, title, date, hour, description }) => {
             <S.Information>
                 <S.InformationTop>
                     <S.Title>{title}</S.Title>
-                    <S.Date><S.CalendarIcon />{date}</S.Date>
-                    <S.Hour><S.ClockIcon />{hour}</S.Hour>
+                    <S.Date><S.CalendarIcon />{moment(day).format('D [de] MMMM, YYYY')}</S.Date>
+                    <S.Hour><S.ClockIcon />{`${initialHour}H - ${finalHour}H`}</S.Hour>
                 </S.InformationTop>
                 <S.InformationBottom>
                     <S.IconDescription />
--- src/pages/Scheduling/index.js
+++ src/pages/Scheduling/index.js
@@ -5,6 +5,7 @@ import SchedualingCard from '../../components/SchedulingCard';
 import * as S from './elements'
 
 const Scheduling = () => {
+
     return (
         <S.Container>
             <S.Title>Exames Concluídos/Andamento</S.Title>
@@ -19,9 +20,9 @@ const Scheduling = () => {
                 </S.VieAllSchedualings>
             </S.SchedulingHeader>
             <S.SchedulingList>
-                <SchedualingCard status='done' title='Exames do servidor C' date='5 de Janeiro, 2021' hour='10H - 12H' description='Nesse Agendamento contem todos os exames do tipo C' />
-                <SchedualingCard status='waiting' title='Exames do servidor B' date='5 de Janeiro, 2021' hour='13H - 15H' description='Nesse Agendamento contem todos os exames do tipo B' />
-                <SchedualingCard status='error' title='Exames do servidor A' date='5 de Janeiro, 2021' hour='7H - 9H' description='Nesse Agendamento contem todos os exames do tipo A' />
+                <SchedualingCard status='done' title='Exames do servidor C' day='2021-01-05' initialHour='10' finalHour='12' description='Nesse Agendamento contem todos os exames do tipo C' />
+                <SchedualingCard status='waiting' title='Exames do servidor B' day='2021-01-05' initialHour='13' finalHour='15' description='Nesse Agendamento contem todos os exames do tipo B' />
+                <SchedualingCard status='error' title='Exames do servidor A' day='2021-01-05' initialHour='7' finalHour='9' description='Nesse Agendamento contem todos os exames do tipo A' />
             </S.SchedulingList>
         </S.Container>
     )
